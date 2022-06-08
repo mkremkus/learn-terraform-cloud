a364412db28c1eca63a230bed20242c516f9e3a0
mkremkus <107073441+mkremkus@users.noreply.github.com>
2022-06-08 15:02:36 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,6 +50,5 @@ resource "google_compute_firewall" "flask" {
 }// A variable for extracting the external IP address of the VM
 output "Web-server-URL" {
  value = join("",["http://",google_compute_instance.default.network_interface.0.access_config.0.nat_ip,":5000"])
-*/
 }
-
+*/
